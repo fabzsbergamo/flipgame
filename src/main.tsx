6ae00cd54cb4a83ff6e5e13c0b6187ecfb048fea
Fabio Sbergamo <fabzsbergamo@gmail.com>
2024-05-04 23:13:39 +0100

    flipgame with emojis

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
-// import App from './App.tsx'
+import App from './App.tsx'
 import EmojiList from './EmojiList.tsx'
 
 const queryClient = new QueryClient();
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    {/* <App /> */}
+    <App />
     <EmojiList/>
     </QueryClientProvider>
   </React.StrictMode>,
